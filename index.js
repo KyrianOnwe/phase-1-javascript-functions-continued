83e46773cd4369b6e7b97fc7fab1ff178d80d9de
Kyrian <dr.kyrian.onwe@gmail.com>
2022-02-25 03:42:06 -0600

    commit tree a06e870818d7cb322ab29e69117d9c6703111479

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Your code here
-function saturdayFun(activity = "roller-skate") {
+/*function saturdayFun(activity = "roller-skate") {
     return `This Saturday, I want to ${activity}!`;
 }
 
@@ -12,4 +12,14 @@ function wrapAdjective(flair = "*") {
         return `You are ${flair}${param}${flair}!`;
     }
     
+}*/
+
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
 }
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(){}
